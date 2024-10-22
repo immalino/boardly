d8d933722ff4c4982cee41afaeccc579dbe1610e
Faza Dandy <fazadandyakmalin0@gmail.com>
2024-10-22 20:06:43 +0700

    building a responsive modal

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { getCurrent } from "@/features/auth/actions";
-import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { getWorkspaces } from "@/features/workspaces/actions";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -7,9 +7,10 @@ export default async function Home() {
 
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="">
-      <CreateWorkspaceForm />
-    </div>
-  );
+  const workspace = await getWorkspaces();
+  if (workspace.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspace.documents[0].$id}`);
+  }
 }
--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -23,12 +23,14 @@ import { useCreateWorkspace } from "../api/use-create-workspace";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
 import { ImageIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface CreateWorkspaceFormProps {
   onCancel?: () => void;
 }
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
+  const router = useRouter();
   const { mutate, isPending } = useCreateWorkspace();
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -49,8 +51,9 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
     mutate(
       { form: finalValues },
       {
-        onSuccess: () => {
+        onSuccess: ({data}) => {
           form.reset();
+          router.push(`/workspaces/${data.$id}`);
         },
       }
     );
