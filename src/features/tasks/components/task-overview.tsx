7ec16b24e3e85bf25fd5c5ffa3042e3f228172fd
immalino <fazadandyakmalin0@gmail.com>
2024-11-11 21:39:37 +0700

    add: edit functionality on task-overview

--- src/features/tasks/components/task-overview.tsx
+++ src/features/tasks/components/task-overview.tsx
@@ -7,18 +7,21 @@ import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { TaskDate } from "./task-date";
 import { Badge } from "@/components/ui/badge";
 import { snakeToTitleCase } from "@/lib/utils";
+import { useEditTaskModal } from "../hooks/use-edit-task-modal";
 
 interface TaskOverviewProps {
   task: Task;
 }
 
 export const TaskOverview = ({ task }: TaskOverviewProps) => {
+  const { open } = useEditTaskModal();
+
   return (
     <div className="flex flex-col gap-y-4 col-span-1">
       <div className="bg-muted rounded-lg p-4">
         <div className="flex items-center justify-between">
           <p className="text-lg font-semibold">Overview</p>
-          <Button variant="secondary" size="sm">
+          <Button onClick={() => open(task.$id)} variant="secondary" size="sm">
             <PencilIcon className="size-4 mr-2" />
             Edit
           </Button>
