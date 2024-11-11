ac92fdff44d4912546e92acda5bc56b0209ec945
immalino <fazadandyakmalin0@gmail.com>
2024-11-11 21:12:42 +0700

    add: delete functionality on task-breadcrumbs

--- src/features/tasks/components/task-breadrumbs.tsx
+++ src/features/tasks/components/task-breadrumbs.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useDeleteTask } from "../api/use-delete-task";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
 
 interface TaskBreadcrumbsProps {
   project: Project;
@@ -13,9 +16,32 @@ interface TaskBreadcrumbsProps {
 
 export const TaskBreadcrumbs = ({ project, task }: TaskBreadcrumbsProps) => {
   const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  const { mutate: deleteTask, isPending } = useDeleteTask();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete task",
+    "This action cannot be undone!",
+    "destructive"
+  );
+
+  const handleDeleteTask = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    deleteTask(
+      { param: { taskId: task.$id } },
+      {
+        onSuccess: () => {
+          router.push(`/workspaces/${workspaceId}/tasks`);
+        },
+      }
+    );
+  };
 
   return (
     <div className="flex items-center gap-x-2">
+      <ConfirmDialog />
       <ProjectAvatar
         name={project.name}
         image={project.imageUrl}
@@ -28,7 +54,13 @@ export const TaskBreadcrumbs = ({ project, task }: TaskBreadcrumbsProps) => {
       </Link>
       <ChevronRightIcon className="size-4 lg:size-5 text-muted-foreground" />
       <p className="text-sm lg:text-lg font-semibold">{task.name}</p>
-      <Button variant="destructive" className="ml-auto" size="sm">
+      <Button
+        onClick={handleDeleteTask}
+        disabled={isPending}
+        variant="destructive"
+        className="ml-auto"
+        size="sm"
+      >
         <TrashIcon className="size-4 lg:mr-2" />
         <span className="hidden lg:block">Delete task</span>
       </Button>
