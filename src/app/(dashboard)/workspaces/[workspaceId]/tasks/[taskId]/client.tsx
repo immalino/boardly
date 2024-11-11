3e180328c267574dc1172fc5700fe95d80207c21
immalino <fazadandyakmalin0@gmail.com>
2024-11-11 21:03:42 +0700

    create: task-breadcrumbs ui

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -3,6 +3,7 @@
 import { PageErrorPage } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadrumbs";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
 const TaskIdClient = () => {
@@ -11,6 +12,10 @@ const TaskIdClient = () => {
 
   if (isLoading) return <PageLoader />;
   if (!task) return <PageErrorPage message="Task not found" />;
-  return <div>TaskIdClient</div>;
+  return (
+    <div className="flex flex-col">
+      <TaskBreadcrumbs project={task.project} task={task} />
+    </div>
+  );
 };
 export default TaskIdClient;
