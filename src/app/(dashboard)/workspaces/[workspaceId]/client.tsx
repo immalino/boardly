3f61456a3e8965532daec6850cce1f0bbd4f2167
immalino <fazadandyakmalin0@gmail.com>
2024-11-19 22:27:12 +0700

    create: project list document

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -18,6 +18,8 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
+import { Project } from "@/features/projects/types";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -34,8 +36,6 @@ export const WorkspaceIdClient = () => {
     workspaceId,
   });
 
-  const { open: createProject } = useCreateProjectModal();
-
   const isLoading =
     isLoadingAnalytics ||
     isLoadingTasks ||
@@ -51,6 +51,7 @@ export const WorkspaceIdClient = () => {
       <Analytics data={analytics} />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <TaskList data={tasks.documents} total={tasks.total} />
+        <ProjectList data={projects.documents} total={projects.total} />
       </div>
     </div>
   );
@@ -69,7 +70,7 @@ export const TaskList = ({ data, total }: TaskListProps) => {
       <div className="bg-muted rounded-lg p-4">
         <div className="flex items-center justify-between">
           <p className="text-lg font-semibold">
-            Task {total > 0 ? `(${total})` : ""}
+            Tasks {total > 0 ? `(${total})` : ""}
           </p>
           <Button onClick={createTask} variant="muted" size="icon">
             <PlusIcon className="size-4 text-neutral-400" />
@@ -105,6 +106,54 @@ export const TaskList = ({ data, total }: TaskListProps) => {
         <Button variant="muted" className="mt-4 w-full" asChild>
           <Link href={`/workspaces/${workspaceId}/tasks`}>Show All</Link>
         </Button>
+      </div>
+    </div>
+  );
+};
+interface ProjectListProps {
+  data: Project[];
+  total: number;
+}
+export const ProjectList = ({ data, total }: ProjectListProps) => {
+  const workspaceId = useWorkspaceId();
+  const { open: createProject } = useCreateProjectModal();
+
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-white border rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">
+            Projects {total > 0 ? `(${total})` : ""}
+          </p>
+          <Button onClick={createProject} variant="secondary" size="icon">
+            <PlusIcon className="size-4 text-neutral-400" />
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {data.map((project) => (
+            <li key={project.$id}>
+              <Link href={`/workspaces/${workspaceId}/projects/${project.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                  <CardContent className="p-4 flex items-center gap-x-2.5">
+                    <ProjectAvatar
+                      image={project.imageUrl}
+                      name={project.name}
+                      className="size-12"
+                      fallbackClassName="text-lg"
+                    />
+                    <p className="text-lg font-medium truncate">
+                      {project.name}
+                    </p>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No projects found
+          </li>
+        </ul>
       </div>
     </div>
   );
